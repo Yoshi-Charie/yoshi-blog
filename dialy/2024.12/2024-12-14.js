eb6055be0b9692ec65b237932310db519bd69b28
Yoshi-Charie <ngfgmg0406@gmail.com>
2024-12-14 07:13:23 +0900

    Initial commit for simple diary blog

---
+++ dialy/2024.12/2024-12-14.js
@@ -0,0 +1,13 @@
+document.body.innerHTML = `
+  <header>
+    <h1>2024年12月14日の日記</h1>
+  </header>
+
+  <main>
+    <p>今日は晴れたので、外に散歩に出かけました。気持ちよかった！</p>
+  </main>
+
+  <footer>
+    <a href="../index.html">戻る</a>
+  </footer>
+`;
