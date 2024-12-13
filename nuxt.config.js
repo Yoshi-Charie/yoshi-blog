11ab72f341adc64a0d52fff17395568d5008895a
Yoshi-Charie <ngfgmg0406@gmail.com>
2024-12-13 20:09:42 +0900

    Add changes to main branch

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  ssr: FALSE, // SSR (サーバーサイドレンダリング) を有効にする
+  ssr: false, // SSR (サーバーサイドレンダリング) を有効にする
 
   nitro: {
     preset: 'static',  // 静的サイトとして生成
