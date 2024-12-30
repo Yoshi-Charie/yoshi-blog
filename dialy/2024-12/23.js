a31d80fab3201daa1bef216e2d2b88e26c6ea76b
Yoshi-Charie <ngfgmg0406@gmail.com>
2024-12-31 08:38:06 +0900

    a

---
+++ dialy/2024-12/23.js
@@ -0,0 +1,18 @@
+   document.body.innerHTML = `
+  <header>
+    <h1>2024年12月23日の日記</h1>
+  </header>
+
+  <main>
+    <p>
+    池山さんとの件が調整の反応が悪く萎えてたが<br>
+    とりあえず1/9でまとまった<br>
+    まぁいいかなくらい<br>
+    行けるのは嬉しいけど流れが良くない<br>
+    </p>
+  </main>
+
+  <footer>
+    <a href="../index.html">戻る</a>
+  </footer>
+`;
---
+++ dialy/2024-12/28.js
@@ -0,0 +1,17 @@
+   document.body.innerHTML = `
+  <header>
+    <h1>2024年12月28日の日記</h1>
+  </header>
+
+  <main>
+    <p>
+    一気に環境構築が進んだ<br>
+    ゲーミングミキサーが届いたので29にマイク一式が届くように注文した<br>
+    ついでに浄水器周りも整備出来て、脱アムウェイが出来そう<br>
+    </p>
+  </main>
+
+  <footer>
+    <a href="../index.html">戻る</a>
+  </footer>
+`;
