fb784283ae0252dfd8f02409943f8815e3123665
Yoshi-Charie <ngfgmg0406@gmail.com>
2025-01-09 14:50:45 +0900

    a

---
+++ dialy/2025-1/7.js
@@ -0,0 +1,18 @@
+   document.body.innerHTML = `
+  <header>
+    <h1>2025年1月7日の日記</h1>
+  </header>
+
+  <main>
+    <p>
+    なんか池山さんとの予定に乗り気になれない。<br>
+    スケジュール調整でごちゃごちゃして、<br>
+    向こうがあまり乗り気でないなと感じてから<br>
+    ただただめんどくさいイベントっぽくなってしまった。<br>
+    </p>
+  </main>
+
+  <footer>
+    <a href="../index.html">戻る</a>
+  </footer>
+`;
