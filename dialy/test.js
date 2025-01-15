15a91650deebb4e65650ef0a92e7b6521db3dad8
Yoshi-Charie <ngfgmg0406@gmail.com>
2025-01-15 19:49:51 +0900

    a

--- dialy/test.js
+++ dialy/test.js
@@ -1,10 +1,11 @@
    document.body.innerHTML = `
   <header>
-    <h1>2024年12月14日の日記</h1>
+    <h1>2025年1月14日の日記</h1>
   </header>
 
   <main>
-    <p>今日は晴れたので、外に散歩に出かけました。気持ちよかった！</p>
+    <p>
+    </p>
   </main>
 
   <footer>
